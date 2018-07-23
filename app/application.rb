49ed112c26f114308085ee449687346dac35954e
Courtney Haltiwanger <chalt0319@gmail.com>
2018-07-23 13:38:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -24,11 +24,10 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["item"]
-
       if @@items.include?(search_term)
         resp.write "added #{search_term}"
       else
-        resp.write "Couldn't find #{search_term}"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
